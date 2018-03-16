5b7757b203316de2fdc40a90c103e730a67c7c84
Curso <Curso@IPARTEKAULA.COM>
2018-03-16 09:52:08 +0100

    Principio

---
+++ src/app/providers/mocks.productos.ts
@@ -0,0 +1,82 @@
+export class MOCKS_Productos {
+
+    static stockproductos = `[
+		{
+		"id": 1,
+		"nombre": "Befeeter ginebra inglesa botella",
+		"precio": 13.75,
+		"imagen": "https://www.fortunaonline.net/media/catalog/product/cache/1/image/650x/040ec09b1e35df139433887a97daa66f/r/s/rsqgin009-beefeater-london-dry-gin-1l.png",
+		"descuento": 20
+	},
+	{
+		"id": 2,
+		"nombre": "Maíz Dulce Lata Fácil Apertura",
+		"precio": 1.75,
+		"imagen": "https://st2.tudespensa.com/rep/7983/imagenes/33952/122/maiz-dulce-bonduelle-lata-facil-apertura-pack3x140-gr-peso-ne.jpg",
+		"descuento": 0
+	},
+	{
+		"id": 3,
+		"nombre": "Tomate Natural Triturado Lata 800 gr",
+		"precio": 3.75,
+		"imagen": "https://st2.tudespensa.com/rep/e1bd/imagenes/37114/122/tomate-natural-triturado-cidacos-lata-800-gr.jpg",
+		"descuento": 0
+	},
+
+	{
+		"id": 4,
+		"nombre": "Espárrago Blanco Ecológico Frasco 205 gr",
+		"precio": 6.75,
+		"imagen": "https://st1.tudespensa.com/rep/9c8c/imagenes/58107/122/esparrago-blanco-ecologico-ekolo.jpg",
+		"descuento":0
+	
+	},
+	{
+		"id": 5,
+		"nombre": "Habitas Tiernas Extra - Extra Finas Frasco 210 gr",
+		"precio": 5.75,
+		"imagen": "https://st2.tudespensa.com/rep/805a/imagenes/41047/122/habitas-tiernas-extra---extra-finas-gvtarra-frasco-210-gr-peso-neto-escurrido.jpg",
+		"descuento": 0
+	},
+	{
+		"id": 6,
+		"nombre": "Arroz Redondo",
+		"precio": 2.75,
+		"imagen": "https://st1.tudespensa.com/rep/9447/imagenes/78261/122/arroz-redondo-sos.jpg",
+		"descuento": 0
+	},
+	{
+		"id": 7,
+		"nombre": "Befeeter ginebra inglesa botella",
+		"precio": 13.75,
+		"imagen": "https://img-global.cpcdn.com/002_recipes/recipes_100641_v1415182910_receta_foto_00100641-qix6cps3auf9q60lfrkm/1200x630cq70/photo.jpg",
+		"descuento": 0
+	},
+	{
+		"id": 8,
+		"nombre": "Ravioli de Jamón y Queso",
+		"precio": 5.75,
+		"imagen": "https://st1.tudespensa.com/rep/4fd6/imagenes/44595/122/giovanni-rana-ravioli-de-jamon-y-queso-bolsa-250-gr.jpg",
+		"descuento": 0
+	},
+	{
+		"id": 9,
+		"nombre": "Garbanzo",
+		"precio": 4.75,
+		"imagen": "https://st1.tudespensa.com/rep/531c/imagenes/77106/122/garbanzo-luengo.jpg",
+		"descuento": 0
+	},
+
+	{
+		"id": 10,
+		"nombre": "Quinoa Ecológica",
+		"precio": 1.75,
+		"imagen": "https://st3.tudespensa.com/rep/2da6/imagenes/136343/122/quinoa-ecologica-ecocesta.jpg",
+		"descuento": 0
+	}
+
+
+	
+]`;
+
+}
