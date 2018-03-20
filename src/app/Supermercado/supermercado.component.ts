0ec862cd7e89f69e7121d37c0e64b4b142c36ff3
Curso <Curso@IPARTEKAULA.COM>
2018-03-20 09:15:24 +0100

    mejor tabulado

--- src/app/Supermercado/supermercado.component.ts
+++ src/app/Supermercado/supermercado.component.ts
@@ -28,21 +28,22 @@ export class SupermercadoComponent implements OnInit
   
   //Metodo para sumar la cantidad de productos que quieres
   sumar(cantidad) {
-            console.log("Click sumar");
-            this.productoscantidad.cantidad= cantidad;
-            this.productoscantidad.cantidad++;
+                    console.log("Click sumar");
+                    this.productoscantidad.cantidad= cantidad;
+                    this.productoscantidad.cantidad++;
           }
   //Metodo para restar la cantidad de productos que quieres
   restar(cantidad) {
              console.log("Click restar");
-
-            //Si la cantidad es igual que 1 no hace nada,si es mayor resta la cantidad 1 vez
+             this.productoscantidad.cantidad= cantidad;
+            
+             //Si la cantidad es igual que 1 no hace nada,si es mayor resta la cantidad 1 vez
             if(this.productoscantidad.cantidad==1)
                {
 
              }
             else{
-              this.productoscantidad.cantidad= cantidad;
+                  this.productoscantidad.cantidad= cantidad;
                   this.productoscantidad.cantidad--;
                 }
     
