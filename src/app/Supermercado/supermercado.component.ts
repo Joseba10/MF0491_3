c7f7b042fd5a2d887af467560675990cc833f060
Curso <Curso@IPARTEKAULA.COM>
2018-03-21 10:44:47 +0100

    Sumar y Restar arreglado

--- src/app/Supermercado/supermercado.component.ts
+++ src/app/Supermercado/supermercado.component.ts
@@ -29,27 +29,27 @@ export class SupermercadoComponent implements OnInit
               }
   
   //Metodo para sumar la cantidad de productos que quieres
-  sumar(cantidad) {
+  sumar(producto) {
                  
-                    this.productoscantidad.cantidad= cantidad;
-                    this.productoscantidad.cantidad++;
-                    console.log("Click sumar", cantidad,this.productoscantidad.cantidad);
+                   
+                producto.cantidad++;
+                    console.log("Click sumar",this.productoscantidad.cantidad);
           }
   //Metodo para restar la cantidad de productos que quieres
-  restar(cantidad) {
+  restar(producto) {
       
-             this.productoscantidad.cantidad= cantidad;
+          
      
             
              //Si la cantidad es igual que 1 no hace nada,si es mayor resta la cantidad 1 vez
-            if(this.productoscantidad.cantidad==1)
+            if( producto.cantidad<=1)
                {
 
              }
             else{
-                  this.productoscantidad.cantidad= cantidad;
-                  this.productoscantidad.cantidad--;
-                  console.log("Click restar",cantidad,this.productoscantidad.cantidad);
+     
+              producto.cantidad--;
+                  console.log("Click restar",this.productoscantidad.cantidad);
                 }
     
           }
